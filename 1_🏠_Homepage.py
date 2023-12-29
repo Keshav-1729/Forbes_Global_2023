3c4a3d1751eff972a14898be0d5cf53e5180a57d
Keshav Mishra <121629545+Keshav-1729@users.noreply.github.com>
2023-12-29 20:42:41 +0530

    Update 1_🏠_Homepage.py

--- 1_🏠_Homepage.py
+++ 1_🏠_Homepage.py
@@ -72,8 +72,8 @@ st.write("---")
 
 l4,r4=st.columns(2)
 with l4:
-    st.markdown("# The final Web Application!:computer:")
-    st.write("Performing all the above steps in sequence and using the required Dependencies, we made it to the final Product ready for the deployment.")
+    st.markdown("# Web Application and its Deployment :computer:")
+    st.write("Performing all the above steps in sequence and using the required Dependencies, we made it to the final Product ready for the deployment on **streamlit cloud**.")
 with r4:
     st_lottie(lottie_coding4,height=300,key="insights3")
 st.write("---")
